40006434085c2aad9ac210e7eb923e52f3aeedb2
rhakhshchasch <alexeypershikov@gmail.com>
2023-02-23 15:25:33 +0600

    Add file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo 'Hello World';
+
+$name = ' and Alexey';
+echo $name;
+
+// $1number = 17; - wrong name for variable, first symbol can't be a number
+
+/*
+multiline commentary
+*/
+
+$number1 = 17;//comment: we use '=' to assign values
+
+// bool, int, str, float
+
+$surname = 'Pershikov';
+
+echo $name . ' ' . $surname; // we use '.' to make concatenation for strings
+
+echo (5 * $name);
