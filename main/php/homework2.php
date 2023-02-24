ec6d779b8fcace32a7be99d17e39d0942c96cbf1
rhakhshchasch <alexeypershikov@gmail.com>
2023-02-25 01:59:25 +0600

    HW 25.02 PHP

---
+++ main/php/homework2.php
@@ -0,0 +1,28 @@
+1. 
+Мы проходили следующие типы данных: 
+- string - строка
+- integer - число (целое)
+- float - число (десятичная дробь)
+- boolean - логический (TRUE or FALSE)
+
+а Google ещё подсказывает
+- array (массивы)
+- object (объекты)
+- resource (ресурсы)
+- NULL ("пустой" тип)
+
+
+2. 
+$a == $b	Equal	                    Равенство
+$a === $b	Identical	                Идентичность, тождественность
+$a != $b	Not equal	                Не равенство
+$a <> $b	Not equal	                Не равенство
+$a !== $b	Not identical	            Не тождественность
+$a < $b	    Less than	                Меньше чем
+$a > $b	    Greater than	            Больше чем
+$a <= $b	Less than or equal to   	Меньше или равно
+$a >= $b	Greater than or equal to	Больше или равно
+$a <=> $b	Spaceship                   Сравнение (??)
+
+Насколько я понял, последний тип показывает результат 
+сравнения двух объектов.
