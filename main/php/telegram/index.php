7632995c48b380db9f38b355ba6e1bed1e3be434
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-02-25 16:10:01 +0400

    edit

---
+++ main/php/telegram/index.php
@@ -0,0 +1,22 @@
+<?php 
+include "controller/telegram.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Telegram</title>
+    <link rel="stylesheet" href="access/styles/style.css">
+</head>
+<body>
+    <form class="form-message" action="" method="POST">
+        <h1>Telegram</h1>
+        <input type="text" name="first_name">
+        <input type="text" name="last_name">
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
