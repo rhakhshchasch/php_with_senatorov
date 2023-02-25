a26dc6f9340be91750910ffc3dbdf5f2acb78ef7
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-02-26 00:09:43 +0400

    add

---
+++ main/php/homework3.php
@@ -0,0 +1,18 @@
+<!--Some notes on GET requests:-->
+<!---->
+<!--$_GET-->
+<!--GET requests can be cached-->
+<!--GET requests remain in the browser history-->
+<!--GET requests can be bookmarked-->
+<!--GET requests should never be used when dealing with sensitive data-->
+<!--GET requests have length restrictions-->
+<!--GET requests are only used to request data (not modify)-->
+<!---->
+<!--Some notes on POST requests:-->
+<!---->
+<!--$_POST-->
+<!--POST requests are never cached-->
+<!--POST requests do not remain in the browser history-->
+<!--POST requests cannot be bookmarked-->
+<!--POST requests have no restrictions on data length-->
+
