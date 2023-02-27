1b34c6fbe09d6dd3b1f1189e5f9804a18e0f9dfc
rhakhshchasch <alexeypershikov@gmail.com>
2023-02-28 02:53:01 +0600

    Euler2

--- main/algorithms/euler.php
+++ main/algorithms/euler.php
@@ -1,3 +1,4 @@
+<!-- 1.  -->
 <?php
 
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -18,3 +19,18 @@ for($i = 1; $i < 1000; $i++){
     }
 }
 echo $sum;
+
+
+// 2. 
+<?php
+$fibo = [1 => 0, 1, 1];
+$sum = 0;
+while($fibo[count($fibo)] <= 4000000){
+    array_push($fibo, $fibo[count($fibo)] + $fibo[count($fibo)-1]);
+} 
+for($i = 1; $i < count($fibo); $i++){
+    if($fibo[$i] % 2 == 0){
+        $sum += $fibo[$i];
+    } 
+} echo $sum;
+?>
