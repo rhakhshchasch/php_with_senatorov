40f228decaf2bbec4726cc4f87a52e01858f67fe
rhakhshchasch <125961837+rhakhshchasch@users.noreply.github.com>
2023-03-31 16:35:06 +0530

    lesson 5
    Начали делать калькулятор

---
+++ main/php/calculator/index.php
@@ -0,0 +1,18 @@
+<?php include 'controller/calculator.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="access/styles/style.css">
+    <script src="access/js/scrypt.js"></script>
+    <title>Calculator</title>
+</head>
+
+<body>
+<h1>Calculator</h1>
+ 
+</body>
+</html>
