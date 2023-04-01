7a7c09e1ba069490ac729a1aa6316071ee6371bf
rhakhshchasch <125961837+rhakhshchasch@users.noreply.github.com>
2023-04-01 16:33:24 +0530

    lesson 6
    Фронт калькулятора и дебаг передачи на бэк

--- main/php/calculator/index.php
+++ main/php/calculator/index.php
@@ -13,6 +13,18 @@
 
 <body>
 <h1>Calculator</h1>
- 
+ <form class="form-message" action="controller/calculator.php" method="post">
+ <label for="num1">Enter first number</label>   
+ <input type="number" name="num1" id=""><br><br>
+ <select name="operator" id="">
+    <option value="+">sum</option>
+    <option value="-">difference</option>
+    <option value="*">multiplication</option>
+    <option value="/">division</option>
+ </select><br><br>
+ <label for="num2">Enter second number</label>   
+ <input type="number" name="num2" id=""><br><br>
+ <input type="submit" value="Result!"><br><br>
+ </form>
 </body>
 </html>
