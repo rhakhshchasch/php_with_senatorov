62fdece3186690b9835f6a636e54d76f62e80145
rhakhshchasch <125961837+rhakhshchasch@users.noreply.github.com>
2023-04-07 21:05:43 +0530

    lesson 07.04.2023
    Начали знакомиться с ALAX. Отправили первый запрос

---
+++ main/php/calculator_function_AJAX/access/js/scrypt.js
@@ -0,0 +1,26 @@
+$.ajax({
+	url: '/controller/calculator.php',         /* Куда пойдет запрос */
+	method: 'post',                              /* Метод передачи (post или get) */
+	dataType: 'json',          /* Тип данных в ответе (xml, json, script, html). */
+	data: {text: 'Текст'},     /* Параметры передаваемые в запросе. */
+	success: function(data){   /* функция которая будет выполнена после успешного запроса.  */
+		console.log(data);     /* В переменной data содержится ответ от index.php. */
+	},
+    error: function (jqXHR, exception) {
+		if (jqXHR.status === 0) {
+			console.log('Not connect. Verify Network.');
+		} else if (jqXHR.status == 404) {
+			console.log('Requested page not found (404).');
+		} else if (jqXHR.status == 500) {
+			console.log('Internal Server Error (500).');
+		} else if (exception === 'parsererror') {
+			console.log('Requested JSON parse failed.');
+		} else if (exception === 'timeout') {
+			console.log('Time out error.');
+		} else if (exception === 'abort') {
+			console.log('Ajax request aborted.');
+		} else {
+			console.log('Uncaught Error. ' + jqXHR.responseText);
+		}
+	}
+});
---
+++ main/php/calculator_function_AJAX/index.php
@@ -0,0 +1,31 @@
+<?php include 'controller/calculator.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="access/styles/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="access/js/scrypt.js"></script>
+    <title>Calculator</title>
+</head>
+
+<body>
+<h1>Calculator</h1>
+ <form class="form-message" action="controller/calculator.php" method="POST">
+ <label for="num1">Enter first number</label>   
+ <input type="text" name="num1" id=""><br><br>
+ <select name="operator" id="">
+    <option value="+">sum</option>
+    <option value="-">difference</option>
+    <option value="*">multiplication</option>
+    <option value="/">division</option>
+ </select><br><br>
+ <label for="num2">Enter second number</label>   
+ <input type="text" name="num2" id=""><br><br>
+ <input type="submit" name="submit" value="Result!"><br><br>
+ </form>
+</body>
+</html>
